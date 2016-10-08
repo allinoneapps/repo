5e079d9653da809179c20823dca637dcee7172bd
adhikari18 <ashok.adhikari18@gmail.com>
2016-10-08 15:56:11 -0400

    more db items for marathi

---
+++ marathi/HowTo-DynamicMenuLinks.js
@@ -0,0 +1,3 @@
+FB212.com->window.JSInterface.LaunchWebView('http://facebook.com')
+MSN.com->window.JSInterface.LaunchWebView('http://www.msn.com',true)
+Test Market->window.JSInterface.LaunchGooglePlay('market://details?id=com.ashokProjects.typeMarathi');
